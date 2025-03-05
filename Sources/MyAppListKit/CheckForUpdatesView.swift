b16e917040e91204580b5bead7e6c27e116c01b0
jaywcjlove <398188662@qq.com>
2025-03-06 04:16:41 +0800

    feat: add `CheckForUpdatesView`.

---
+++ Sources/MyAppListKit/CheckForUpdatesView.swift
@@ -0,0 +1,24 @@
+//
+//  CheckForUpdatesView.swift
+//  MyAppListKit
+//
+//  Created by 王楚江 on 2025/3/6.
+//
+
+import SwiftUI
+
+public extension MyAppList.AppData {
+    struct CheckForUpdatesView: View {
+        public init(app: MyAppList.AppData) {
+            self.app = app
+        }
+        var app: MyAppList.AppData
+        public var body: some View {
+            Button(action: {
+                app.openURL()
+            }, label: {
+                Text("check_for_updates".localized())
+            })
+        }
+    }
+}
