a908e01d53946fdb7a25d8db53cb678dcdda5486
jaywcjlove <398188662@qq.com>
2025-03-06 14:30:25 +0800

    feat: update MyAppCheckForUpdatesView

--- Sources/MyAppListKit/MyAppCheckForUpdatesView.swift
+++ Sources/MyAppListKit/MyAppCheckForUpdatesView.swift
@@ -7,16 +7,21 @@
 
 import SwiftUI
 
-public struct MyAppCheckForUpdatesView: View {
-    public init(app: MyAppList.AppData) {
+public struct MyAppCheckForUpdatesView<BeforeView: View, AfterView: View>: View {
+    public init(app: MyAppList.AppData, before: BeforeView? = nil, after: AfterView?) {
         self.app = app
     }
+    /// chevron.right
+    var before: BeforeView?
+    var after: AfterView?
     var app: MyAppList.AppData
     public var body: some View {
         Button(action: {
             app.openURL()
         }, label: {
+            before
             Text("check_for_updates".localized())
+            after
         })
     }
 }
