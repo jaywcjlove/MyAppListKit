71a6066075d32ae2fe346f6fdb8704a2e306466f
小弟调调 <398188662@qq.com>
2025-03-18 10:55:49 +0800

    fix: fix ButtonSendFeedback issue.

--- Sources/MyAppListKit/ButtonSendFeedback.swift
+++ Sources/MyAppListKit/ButtonSendFeedback.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 public struct ButtonSendFeedback: View {
     @Environment(\.locale) var locale
-    public init() {}
+    var app: MyAppList.AppData
+    public init(app: MyAppList.AppData) {
+        self.app = app
+    }
     public static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
     public var body: some View {
         Button("send_feedback".localized(locale: locale)) {
-            let emailURLString: String = MyAppList.appRightMenuMaster.sendFeedback(content: MyAppList.version)
+            let emailURLString: String = app.sendFeedback(content: MyAppList.version)
             MyAppList.openURL(string: emailURLString)
         }
     }
