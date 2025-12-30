3fab896fc276cb79e00010ad9896eeaa53978b88
小弟调调 <398188662@qq.com>
2025-12-31 03:33:27 +0800

    feat: Enhance the localized function.

--- Sources/MyAppListKit/Extensions/Extensions.swift
+++ Sources/MyAppListKit/Extensions/Extensions.swift
@@ -18,17 +18,19 @@ public extension String {
     static func localized(key: String, bundle: Bundle? = nil, comment: String = "", locale: Locale, _ arguments: any CVarArg...) -> String {
         let bd = bundle ?? .module
         guard let path = bd.path(forResource: locale.identifier, ofType: "lproj"),
-              let bundle = Bundle(path: path) else {
+              let localeBundle = Bundle(path: path) else {
             let format = NSLocalizedString(key, bundle: bd, comment: comment)
             return String.localizedStringWithFormat(format, arguments)
         }
-        let format = NSLocalizedString(key, bundle: bundle, comment: comment)
+        let format = NSLocalizedString(key, bundle: localeBundle, comment: comment)
         return String.localizedStringWithFormat(format, arguments)
     }
-    static func localized(key: String, bundle: Bundle? = nil, comment: String = "", locale: String, _ arguments: any CVarArg...) -> String {
-        self.localized(key: key, bundle: bundle, locale: .init(identifier: locale), arguments)
+    static func localized(key: String, bundle: Bundle? = nil, comment: String = "", locale: String, _ arguments: CVarArg...) -> String {
+        return self.localized(key: key, bundle: bundle, comment: comment, locale: Locale(identifier: locale), arguments)
     }
-    
+    static func localized(key: String, bundle: Bundle? = nil, locale: String, _ arguments: CVarArg...) -> String {
+        return self.localized(key: key, bundle: bundle, locale: Locale(identifier: locale), arguments)
+    }
     func localized(bundle: Bundle? = nil, comment: String = "") -> String {
         return NSLocalizedString(self, bundle: bundle ?? .module, comment: comment)
     }
